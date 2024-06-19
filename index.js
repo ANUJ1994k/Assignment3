fa45ddb4cba4b468e1bc4c358e9d034b9471f2ce
anuj1994k <anuj1994k@gmail.com>
2024-06-19 10:26:10 +0530

    first ready for pushing

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+function getUserData(id,callback){
+setTimeout(()=>{
+// Create a dummy user object
+const user = {
+    id: "1",
+    name: 'ANUJ KUMAR',
+    email: 'anuj1994k@gmail.com'
+  };
+  // Calling the callback function with the user object
+  callback(user);
+},2000);
+}
+
+// Defining  the displayUser function
+
+function displayUser(user){
+    console.log('User ID:', user.id);
+  console.log('Name:', user.name);
+  console.log('Email:', user.email);
+}
+
+//Calling the getUserData function with a user ID and the displayUser function as the callback
+getUserData(1, displayUser);
